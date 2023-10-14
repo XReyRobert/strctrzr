1b21b9d365e346252f6738acf2b09a40a7a6ce69
xreyrobert-ibm <reyrobert@fr.ibm.com>
2023-10-14 16:39:17 +0200

    update to dark and light themes using the 20231014 App Icon library scrapping

--- convert_svg.sh
+++
@@ -1,6 +0,0 @@
-#qlmanage quick and good on macos but will use a colored background
-#qlmanage -t -s 48 -o png svg/*
-
-#mogrify working fine but way slower (imagemagic/inkscape) with transparent background
-
-mogrify -format png -resize 256x256 -background none -density 1200 -path ./png/ svg/*.svg
